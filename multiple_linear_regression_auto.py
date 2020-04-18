7fe15e4b0d5029093d60e00f128eba24d75dd9b6
huseyinsalis <hs129@zips.uakron.edu>
2020-04-18 01:08:55 -0400

    Update multiple_linear_regression_auto.py

--- multiple_linear_regression_auto.py
+++ multiple_linear_regression_auto.py
@@ -17,7 +17,7 @@ from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 
-#X=X[:,1:]
+X=X[:,1:]
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
@@ -34,14 +34,17 @@ def backwardElimination(x, sl):
     for i in range(0, numVars):
         regressor_OLS = sm.OLS(y, x).fit()
         maxVar = max(regressor_OLS.pvalues).astype(float)
+        print(regressor_OLS.pvalues)
         if maxVar > sl:
             for j in range(0, numVars - i):
                 if (regressor_OLS.pvalues[j].astype(float) == maxVar):
                     x = np.delete(x, j, 1)
+            #print(regressor_OLS.pvalues[j].astype(float))
     regressor_OLS.summary()
     return x
  
 SL = 0.05
+X=np.append(arr=np.ones((50,1)).astype(int),values=X,axis=1)
 X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 X_opt=np.array(X_opt,dtype=float)
 X_Modeled = backwardElimination(X_opt, SL)
