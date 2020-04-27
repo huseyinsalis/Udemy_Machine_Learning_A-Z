3f5f2ca29d83bc65beb6db55d63c17204ff98edb
huseyinsalis <hs129@zips.uakron.edu>
2020-04-27 17:32:29 -0400

    last onee

---
+++ RandomForestRegressionC.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 23 15:19:58 2020
+
+@author: user1
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('CableImpedances.csv')
+X = dataset.iloc[:, 0:-1].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.ensemble import RandomForestRegressor
+regressor=RandomForestRegressor(n_estimators=300)
+regressor.fit(X,y)
+
+y_pred= regressor.predict([[6.5]])
+
+#Visualising Random Forest Regression results
+#This one is wrong because DTRM is not continues
+plt.scatter(X, y, color = 'red')
+plt.plot(X, regressor.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Random Forest)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Random Forest Regression')
+plt.xlabel('Length')
+plt.ylabel('Impedance')
+plt.show()
