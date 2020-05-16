37b69d093f56d6f4c95d022beacc80401815a382
huseyinsalis <hs129@zips.uakron.edu>
2020-05-15 21:12:14 -0400

    last

--- RandomForestRegression.py
+++ RandomForestRegression.py
@@ -9,7 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('CableImpedances.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
