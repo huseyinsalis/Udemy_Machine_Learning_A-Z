01138156b81fde344fb87a02b561d90b36f2ce1d
huseyinsalis <hs129@zips.uakron.edu>
2020-05-21 18:04:25 -0400

    New CNN and Dataset

---
+++ artificial_neural_network.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 22:13:11 2020
+ ANN example
+@author: user1
+"""
+
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+dataset = pd.read_csv('Churn_Modelling.csv')
