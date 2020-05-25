4ce4198e51da6107a023ca6e1668b4f52cb279d8
huseyinsalis <hs129@zips.uakron.edu>
2020-05-25 14:38:09 -0400

    udemy Machine A-Z finished

--- artificial_neural_network.py
+++ artificial_neural_network.py
@@ -10,3 +10,40 @@ import pandas as pd
 import tensorflow as tf
 
 dataset = pd.read_csv('Churn_Modelling.csv')
+X = dataset.iloc[:, 3 : 13].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1,2])], remainder='passthrough')
+X1 = np.array(ct.fit_transform(X))
+
+# Remove first and third column to avoid from dummy variable trap
+
+X = X1[:,1:]
+idxs = list(range(0, 12))
+idxs.pop(2) #this removes 2 from the list
+X = X[:, idxs]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# ANN
+
+import keras
+
+from keras.models import Sequential
+from keras.layers import Dense
+
+#initilize ANN
+classifier = Sequential()
+
+#add first layer and hidden layer
+
+classifier.add(Dense(6, input_shape=(11,) ,kernel_initializer='uniform', activation = 'relu'))
+
---
+++ convolutional_neural_network.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 23 10:18:40 2020
+
+@author: user1
+"""
+
